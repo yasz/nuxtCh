69e294d53675d5f29c10e36c05bfacf0ae431e9d
peter.yang <yangjiahao@outlook.com>
2020-03-04 09:08:50 +0800

    defultValue优化

--- components/Form.vue
+++ components/Form.vue
@@ -1,29 +1,14 @@
 <script>
-import moment from "moment";
-import { addressOptions } from "~/assets/js/global";
-import applyPhoto from "~/components/2.admissions/applyPhoto.vue";
+import moment from 'moment'
+import { addressOptions } from '~/assets/js/global'
+import applyPhoto from '~/components/2.admissions/applyPhoto.vue'
 
 function parseAntForm(h, data) {
-  let rs = [];
+  let rs = []
   data.forEach(item => {
-    if (item.type == "text") {
+    if (item.type == 'text') {
       rs.push(
         <a-input
-          defaultValue={item.text}
-          // style="width: 50%"
-          v-decorator={[
-            item.text,
-            {
-              rules: [{ required: true, message: "不能为空!" }]
-            }
-          ]}
-          placeholder={item.text}
-        />
-      );
-    } else if (item.type == "phone") {
-      rs.push(
-        <a-input
-          defaultValue={item.text}
           // style="width: 50%"
           v-decorator={[
             item.text,
@@ -31,36 +16,51 @@ function parseAntForm(h, data) {
               rules: [
                 {
                   required: true,
-                  message: "只允许输入数字!",
-                  pattern: new RegExp(/^[1-9]\d*$/, "g"),
-                  len: 15
+                  message: '不能为空!',
+                  initialValue: item.text
+                }
+              ]
+            }
+          ]}
+          placeholder={item.text}
+        />
+      )
+    } else if (item.type == 'phone') {
+      rs.push(
+        <a-input
+          v-decorator={[
+            item.text,
+            {
+              rules: [
+                {
+                  required: true,
+                  message: '只允许输入数字!',
+                  pattern: new RegExp(/^[1-9]\d*$/, 'g'),
+                  len: 15,
+                  initialValue: item.text
                 }
               ],
               getValueFromEvent: event => {
-                return event.target.value.replace(/\D/g, "");
+                return event.target.value.replace(/\D/g, '')
               }
             }
           ]}
           placeholder={item.text}
         />
-      );
-    } else if (item.type == "radio") {
-      let radioGroups = [];
-      let radioDefaultValue = "";
+      )
+    } else if (item.type == 'radio') {
+      let radioGroups = []
+      let radioDefaultValue = ''
       if (item.options instanceof Array) {
         // debugger
-        radioDefaultValue = item.options[0];
+        radioDefaultValue = item.options[0]
         item.options.forEach(item => {
-          radioGroups.push(
-            <a-radio-button value={item}>{item}</a-radio-button>
-          );
-        });
+          radioGroups.push(<a-radio-button value={item}>{item}</a-radio-button>)
+        })
       } else {
-        radioDefaultValue = Object.keys(item.options)[0];
+        radioDefaultValue = Object.keys(item.options)[0]
         for (let [key, value] of Object.entries(item.options)) {
-          radioGroups.push(
-            <a-radio-button value={key}>{value}</a-radio-button>
-          );
+          radioGroups.push(<a-radio-button value={key}>{value}</a-radio-button>)
         }
       }
       rs.push(
@@ -72,58 +72,55 @@ function parseAntForm(h, data) {
                 initialValue: radioDefaultValue
               }
             ]}
-            defaultValue={radioDefaultValue}
             buttonStyle="solid"
           >
             {radioGroups}
           </a-radio-group>
         </a-form-item>
-      );
-    } else if (item.type == "datepicker") {
+      )
+    } else if (item.type == 'datepicker') {
       rs.push(
         <a-date-picker
           v-decorator={[
             item.text,
             {
-              initialValue: moment("2010/01/01", "YYYY/MM/DD")
+              initialValue: moment('2010/01/01', 'YYYY/MM/DD')
             }
           ]}
-          defaultValue={moment("2010/01/01", "YYYY/MM/DD")}
         />
-      );
-    } else if (item.type == "cnCascader") {
+      )
+    } else if (item.type == 'cnCascader') {
       rs.push(
         <a-cascader
           v-decorator={[
             item.text,
             {
-              initialValue: ["浙江省", "温州市", "鹿城区"]
+              initialValue: ['浙江省', '温州市', '鹿城区']
             }
           ]}
           options={addressOptions}
-          defaultValue={["浙江省", "温州市", "鹿城区"]}
           // style="width: 50%"
         />
-      );
-    } else if (item.type == "photo") {
-      rs.push(<applyPhoto />);
-    } else if (item.type == "checkbox") {
-      let groups = [];
-      let defaultValue = "";
+      )
+    } else if (item.type == 'photo') {
+      rs.push(<applyPhoto />)
+    } else if (item.type == 'checkbox') {
+      let groups = []
+      let defaultValue = ''
       if (item.options instanceof Array) {
         // debugger
-        defaultValue = item.options[0];
+        defaultValue = item.options[0]
         item.options.forEach(item => {
-          groups.push(<a-checkbox value={item}>{item}</a-checkbox>);
-        });
+          groups.push(<a-checkbox value={item}>{item}</a-checkbox>)
+        })
       } else {
-        defaultValue = Object.keys(item.options)[0];
+        defaultValue = Object.keys(item.options)[0]
         for (let [key, value] of Object.entries(item.options)) {
           groups.push(
             <a-checkbox default-checked={true} value={key}>
               {value}
             </a-checkbox>
-          );
+          )
         }
       }
       rs.push(
@@ -134,16 +131,15 @@ function parseAntForm(h, data) {
               initialValue: [defaultValue]
             }
           ]}
-          defaultValue={[defaultValue]}
           buttonStyle="solid"
         >
           {groups}
         </a-checkbox-group>
-      );
+      )
     }
-  });
+  })
   //布局
-  let rs2 = [];
+  let rs2 = []
   data.forEach((item, i) => {
     rs2.push(
       <a-form-item
@@ -153,9 +149,9 @@ function parseAntForm(h, data) {
       >
         {rs[i]}
       </a-form-item>
-    );
-  });
-  return rs2;
+    )
+  })
+  return rs2
 }
 export default {
   components: {
@@ -165,9 +161,9 @@ export default {
   //   return { form: this.$form.createForm(this) };
   // },
   parseAntForm,
-  props: ["formData", "form"],
+  props: ['formData', 'form'],
   render: function(h) {
-    return <div>{this.parseAntForm(h, this.formData)}</div>;
+    return <div>{this.parseAntForm(h, this.formData)}</div>
     //  return <div>{this.parseAntForm(h, this.formData)}</div>;
   },
   methods: {
@@ -176,5 +172,5 @@ export default {
     },
     parseAntForm
   }
-};
+}
 </script>
